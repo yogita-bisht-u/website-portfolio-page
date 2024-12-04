9a44d4a828bf732ed449692bd265e6978355b5ec
Yogita bisht <yogitabisht.4april@gmail.com>
2024-12-04 12:09:19 +0530

    updated website

---
+++ about.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+
+
+  <!--
+    - favicon
+  -->
+  <link rel="shortcut icon" href="./assets/images/favicon.ico" type="image/x-icon">
+
+  <!--
+    - custom css link
+  -->
+  <link rel="stylesheet" href="./assets/css/style.css">
+
+  <!--
+    - google font link
+  -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet">
+</head>
+<body class="light-theme"> 
+
+    <?php include 'header.php';?>
+
+    <main>
+<div class="main">
+<div class="container">
+    <h1> this is about page </h1>
+</div>
+</div>
+</main>
+ 
+ 
+    <?php include 'footer.php';?>
+
+    
+</body>
+</html>
